730984fa503f812e3d9ceb03c1d29282fe73404c
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-08-02 13:26:06 -0300

    Add SCSS loader

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,5 @@
 // Import stylesheets
-import '../style.css';
+import '../style.scss';
 // Import ATM
 import { withdraw } from '../atm';
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require("path");
 module.exports = {
   // Change to your "entry-point".
   entry: "./src/index",
+  mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "app.bundle.js"
@@ -12,6 +13,14 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
       {
         // Include ts, tsx, and js files.
         test: /\.(tsx?)|(js)$/,
